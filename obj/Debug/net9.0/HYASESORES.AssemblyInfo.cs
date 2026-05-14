558e623a88ede92484bd6bd8b55b13c7080ff0ab
OskarCreate <oskar_morales_f@outlook.com>
2026-05-13 23:56:51 -0500

    nuevos cambios

--- obj/Debug/net9.0/HYASESORES.AssemblyInfo.cs
+++ obj/Debug/net9.0/HYASESORES.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HYASESORES")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e994b6f4ec1bc90252f5d1fae49d90a3951f16e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c58a37616ec7cbc2dc52a17ce0a6cbaaf983fc8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HYASESORES")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HYASESORES")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
